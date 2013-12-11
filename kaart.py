da08ba1ddf0050a8cb3f28c1e0dce5998427f6f8
Aigar Vetemäe <aigar@aigar-HP-Pavilion-g6-Notebook-PC.(none)>
2013-12-12 00:18:32 +0200

    Initial commit

---
+++ kaart.py
@@ -0,0 +1,30 @@
+kaart=[
+"#################",
+"######******#####",
+"#####***#########",
+"####****######***",
+"#############****",
+]
+
+
+
+
+
+
+
+
+def kaart_print():
+	for rida in kaart:
+		print rida
+
+	
+
+kaart_print()
+
+"""
+import os
+os.system("clear")
+"""
+
+
+def kaart_xy()
